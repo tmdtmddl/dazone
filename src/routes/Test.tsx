8c7e72583f00feb4ee2a010ff3c912098edd07e2
tmdtmddl <tmddlqkqh@naver.com>
2025-03-20 15:36:22 +0900

    md done

--- src/routes/Test.tsx
+++ src/routes/Test.tsx
@@ -1,13 +1,23 @@
-import React from "react";
-import { create } from "zustand";
-
-interface Store {
-  count: number;
-  increase: Func;
-}
+import { storage } from "../lib/firebase";
+import { getDownloadURL } from "firebase/storage";
 
 const Test = () => {
-  return <div>Test</div>;
+  const increase = async () => {
+    const uid = "KvpX0lWPDlSATP8jq6a7P1hJ0682";
+    const id = "0603f0b3-4643-4290-862e-09051761341f";
+    const ref = storage.ref(`${uid}/products/img1`);
+    console.log(ref);
+
+    const url = await getDownloadURL(ref);
+
+    console.log(url);
+  };
+  return (
+    <div>
+      {/* <h1>{count}</h1> */}
+      <button onClick={increase}>Increase</button>
+    </div>
+  );
 };
 
 export default Test;
